95e4a5c1e3c355c2829cab507630d204d69865ad
Elvis <elvisgt1999@gmail.com>
2023-10-08 17:21:47 +0200

    Mostrando la figura

--- src/components/ButtonItem.jsx
+++ src/components/ButtonItem.jsx
@@ -2,10 +2,16 @@ import { useState } from "react"
 import "../assets/styles/button.css"
 
 export const ButtonItem = () => {
-    const [fig,setFig] = useState('X');
+    const [buttonClass,setButtonClass] = useState('buttonFig-text');
+    
+    const handleButton = () => {
+        setButtonClass('buttonFig-text-active')
+    }
+    
     return (
-        <button className="buttonFig">
-            {fig}
+        <button className="buttonFig" onClick={handleButton}>
+            <span className={buttonClass}>X</span>
+            <span className={buttonClass}>O</span>
         </button>
 
     )
